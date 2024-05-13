21a9b70a631ffa0ef607b95880e212b09fd5564e
Senthuran12 <148048451+Senthuran12@users.noreply.github.com>
2024-05-13 15:22:22 +0100

    updating book on other scene

--- Lost Luminous/Assets/Scripts/PauseMenu/PauseMenu.cs
+++ Lost Luminous/Assets/Scripts/PauseMenu/PauseMenu.cs
@@ -4,15 +4,50 @@ using UnityEngine;
 
 public class PauseMenu : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
+    public static bool GameIsPaused = false;
+
+    public GameObject pauseMenuUI;
+
+    private Animator pauseAnimator;
+
+    private void Start()
+    {
+        pauseAnimator = GameObject.Find("pause_Menu_UI").GetComponent<Animator>();
+
+    }
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if(GameIsPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+
+    }
+
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPaused = false;
         
+
+    }
+
+    void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsPaused = true;
+        pauseAnimator.SetBool("flipping book", true);
+
+
     }
 }
