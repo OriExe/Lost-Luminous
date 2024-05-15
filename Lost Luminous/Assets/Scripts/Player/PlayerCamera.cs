3e132373eef3ba127d317fae1213b65823c7fbbd
Azam F <115250689+OriExe@users.noreply.github.com>
2024-05-15 04:50:01 +0100

    ssss

--- Lost Luminous/Assets/Scripts/Player/PlayerCamera.cs
+++ Lost Luminous/Assets/Scripts/Player/PlayerCamera.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 
 public class PlayerCamera : MonoBehaviour
 {
+    private Transform _camera;
+    [SerializeField] private Vector3 offset;
     // Start is called before the first frame update
     void Start()
     {
-        
+        _camera = Camera.main.transform;        
     }
 
     // Update is called once per frame
